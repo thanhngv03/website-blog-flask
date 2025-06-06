59d22f436d81edd8a7f3de965ae31516528f4676
thanhkhon03 <2121050666@student.humg.edu.vn>
2025-06-07 02:45:32 +0700

    create edit,delete

--- app/blog/routes.py
+++ app/blog/routes.py
@@ -30,3 +30,38 @@ def create():
 def detail(post_id):
     post = Post.query.get_or_404(post_id)
     return render_template('blog/detail.html', post=post)
+
+@blog.route('/post/<int:post_id>/edit', methods=['GET', 'POST'])
+@login_required
+def edit(post_id):
+    post = Post.query.get_or_404(post_id)
+
+    if post.user_id != current_user.id:
+        flash("Bạn không có quyền chỉnh sửa bài viết này.", "danger")
+        return redirect(url_for('blog.detail', post_id=post.id))
+
+    form = PostForm(obj=post)
+
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash("Bài viết đã được cập nhật.", "success")
+        return redirect(url_for('blog.detail', post_id=post.id))
+
+    return render_template('blog/edit.html', form=form, post=post)
+
+
+@blog.route('/post/<int:post_id>/delete', methods=['POST'])
+@login_required
+def delete(post_id):
+    post = Post.query.get_or_404(post_id)
+
+    if post.user_id != current_user.id:
+        flash("Bạn không có quyền xoá bài viết này.", "danger")
+        return redirect(url_for('blog.detail', post_id=post.id))
+
+    db.session.delete(post)
+    db.session.commit()
+    flash("Bài viết đã được xoá.", "info")
+    return redirect(url_for('blog.index'))
